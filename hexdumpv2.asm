d73b8bd41bc1cd663765a35a3ff9b7e158f882fc
B.Billion <buhebillion@gmail.com>
2025-02-20 07:49:53 +0200

    DATA SECTIONS

--- hexdumpv2.asm
+++ hexdumpv2.asm
@@ -6,3 +6,32 @@
 ;                         :   from stdin.
 ;
 ;
+
+SECTION .bss              ;   Section containing uninitialised data
+
+        BUFFLEN EQU 10h
+        BUFF: RESB BUFFLEN
+
+SECTION .data             ;   Section containing initialised data
+
+;Here we have two parts of a single useful data structure, implementing
+;the text line of a hex dump utility. The first part displays 16 bytes
+;in hex separated by spaces. Immediately following is a 16‐­
+;character
+;line delimited by vertical bar characters. Because they are adjacent,
+;the two parts can be referenced separately or as a single contiguous
+;unit. If DumpLine is to be used separately, you must
+;append an EOL before sending it to the Linux console.
+
+        DUMPLINE:          DB " 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 "
+        DUMPLEN            EQU $-DUMPLINE
+        ASCLINE:           DB "|................|",10
+        ASCLEN             EQU $-ASCLINE
+        FULLLEN            EQU $-DUMPLINE
+
+
+        SYS_WRITE_CALL_VAL EQU 1
+        STDERR_FD          EQU 2
+        SYS_READ_CALL_VAL  EQU 0
+        STDIN_FD           EQU 0
+        STDOUT_FD          EQU 1
