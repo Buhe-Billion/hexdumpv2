163fc5c47e3bd91bada6d66da5a62092f278c19d
B.Billion <buhebillion@gmail.com>
2025-02-20 07:26:52 +0200

    comments

---
+++ hexdumpv2.asm
@@ -0,0 +1,8 @@
+;NASM                     :   2.14.02
+;Architecture             :   X86-64
+;CPU                      :   Intel® Core™2 Duo CPU T6570 @ 2.10GHz × 2
+;                         :
+;Description              :   This program creates a hex number output [0123456789ABCDEF]
+;                         :   from stdin.
+;
+;
